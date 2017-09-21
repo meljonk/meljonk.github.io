6eb76e2b846e561db9a39b03e270e71b7a1d435a
meljonk <freethink2day@yahoo.com>
2017-09-21 11:44:56 -0700

    updated js loop structure

--- PixelArt Maker/designs.js
+++ PixelArt Maker/designs.js
@@ -1,24 +1,21 @@
-const grid = document.getElementById("pixel-canvas");
+var grid = document.getElementById("pixel-canvas");
 
 
 document.getElementById("gridButton").addEventListener("click", function makeGrid(event) {
     event.preventDefault();
     grid.innerHTML = "";
-  // Select size input and set width and height variables
-    const width = document.getElementById("input-width").value;
-    const height = document.getElementById("input-height").value;
+  // Select size input and set to width and height variables
+    var width = document.getElementById("input-width").value;
+    var height = document.getElementById("input-height").value;
   //Use height and width inputs to build a new grid
     for (let h = 0; h < height; h++) {
-        const rows = document.createElement("tr");
-            let w = 0;
-            while (w < width) {
-                const cells = document.createElement("td");
-                //add event listener on dynamically created cells to select and assign colors
+        var rows = document.createElement("tr");
+            for (let w = 0; w < width; w++) {
+                var cells = document.createElement("td");
                 rows.appendChild(cells).addEventListener("click", function() {
-                const color = document.getElementById("colorPicker").value;
+                var color = document.getElementById("colorPicker").value;
                 this.style.backgroundColor = color;
                 });
-                w++;
             }
             grid.appendChild(rows);
     }
